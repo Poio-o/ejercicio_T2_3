a2712c29c354d0663bef3e09abe3ebd303c2e1d5
Miguel TR <migueltr.2019@outlook.com>
2025-10-01 14:03:40 +0200

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String nombre = "Miguel Trujillo Rojas";
+        String direccion = "Calle Rodahuevos, 69 - Guaro (Málaga) - España";
+        String telefono = "555 123456";
+
+        System.out.printf("""
+            %s
+            %s
+            %s
+        """,nombre,direccion,telefono);
     }
 }
